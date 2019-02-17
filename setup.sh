4fe41b33a9f181a7449af400d761da69afb28b85
denglert <englert.david@gmail.com>
2019-02-17 12:35:08 +0000

    toolbox submodule, setup scripts

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 
-export PROJECT_DIR=$(pwd)
+BASEDIR=$(dirname $(realpath "$BASH_SOURCE"))
+export MACHINELEARNING_SANDBOX_DIR=${BASEDIR}
 
-PYTHON_MODULES_DIR=${PROJECT_DIR}/python/modules/
+PYTHON_MODULES_DIR=${MACHINELEARNING_SANDBOX_DIR}/python/modules/
 export PYTHONPATH=${PYTHON_MODULES_DIR}:${PYTHONPATH}
+
+# - Source toolbox
+source ${MACHINELEARNING_SANDBOX_DIR}/toolbox/setup.sh
